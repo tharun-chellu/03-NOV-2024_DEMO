517ace21808312e371adb61bc8fe7e7353dc5ee1
INFINEON\Chellu <chellu.external@Infineon.com>
2024-11-20 18:44:49 +0530

    adding if elese, elese if conditions_with new file

---
+++ newCode.js
@@ -0,0 +1,8 @@
+let rank = -2+1
+if (rank > 0){
+    console.log(rank + " is a postive number ");
+}else if (rank < 0) {
+    console.log (rank +" is negetive number");
+}else{
+    console.log (rank +" It is zero number");
+}
