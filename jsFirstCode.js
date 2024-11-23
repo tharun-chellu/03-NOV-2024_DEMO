0135f598ac7688a40f5897280536f207c2fbcd7a
INFINEON\Chellu <chellu.external@Infineon.com>
2024-11-23 22:25:09 +0530

    adding 2 files with area of triangle code

--- jsFirstCode.js
+++ jsFirstCode.js
@@ -32,3 +32,19 @@ let i = 7;
 i += 5; // i = i+5 
 console.log('Value  for assignment : '+i)
 
+a=20, b=30, c=40;
+console.log(a+b+c/3);
+
+
+// let base = 12;
+// let height = 15;
+// let A = 1/2 × base × height
+// console.log(A);
+
+const baseValue = 12;
+const heightValue = 15;
+
+// calculate the area
+const areaValue = (baseValue * heightValue) / 2;
+
+console.log(`The area of the triangle is ${areaValue}`);
