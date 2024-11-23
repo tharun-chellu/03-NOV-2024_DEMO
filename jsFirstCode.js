aab266a5c900c2f51243fd21234d282f1c9ac962
INFINEON\Chellu <chellu.external@Infineon.com>
2024-11-23 14:54:50 +0530

    adding js opeators 223_11_2024

--- jsFirstCode.js
+++ jsFirstCode.js
@@ -11,3 +11,24 @@ console.log(countries[1])
 
 let userName  = "kumar";
 console.log(userName);
+
+//addition of 2 number
+/*
+a = 2, b=5 , a+b = 7
+*/
+let a = 5;
+let b = 2;
+console.log( a=b);
+
+let assignmentOperator = 5;
+console.log(assignmentOperator);
+
+let c = 5;
+c  -= 3;
+console.log('value of c is :' +c);
+
+let i = 7;
+
+i += 5; // i = i+5 
+console.log('Value  for assignment : '+i)
+
